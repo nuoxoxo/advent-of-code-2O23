a1dd729822aebbacec4a13c5a4665051780e8edc
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-11-29 22:59:17 +0100

    py : no bs

--- template.py
+++ template.py
@@ -2,24 +2,15 @@ import requests
 import sys
 import os
  
+day = 1
 session = os.getenv('AOC_SESSION')
-day = 3
-
-print(session)
-
-def get_input(day):
-    url = "https://adventofcode.com/2022/day/" + str(day) + "/input"
-    headers = {'Cookie': 'session=' + session}
-    resp = requests.get(url, headers = headers)
-    if resp.status_code != 200:
-        sys.exit(f'{resp.status_code} ... {resp.reason} ... {resp.content}')
-    return resp.text
-
-def main():
-    infile = get_input(day).split('\n')
-    infile.pop()
-    print(infile, f'len: {len(infile)}')
-
-if __name__ == "__main__":
-    main()
+url = "https://adventofcode.com/2022/day/" + str(day) + "/input"
+headers = {'Cookie': 'session=' + session}
+resp = requests.get(url, headers = headers)
+if resp.status_code != 200:
+    sys.exit(f'{resp.status_code} ... {resp.reason} ... {resp.content}')
+infile = resp.text
+lines = infile.split('\n')
+lines.pop()
+print(lines, 'len:', len(lines))
 
