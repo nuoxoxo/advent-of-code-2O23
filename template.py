6ca1174bd6ffe66e0f57d81e76e1d1e9ac0f0ede
nuo.o <49533950+nuoxoxo@users.noreply.github.com>
2023-11-30 14:18:43 +0100

    Update template.py

--- template.py
+++ template.py
@@ -8,7 +8,7 @@ url = "https://adventofcode.com/2022/day/" + str(day) + "/input"
 headers = {'Cookie': 'session=' + session}
 resp = requests.get(url, headers = headers)
 if resp.status_code != 200:
-    sys.exit(f'{resp.status_code} ... {resp.reason} ... {resp.content}')
+    sys.exit(f'{resp.status_code}\n{resp.reason}\n{resp.content}')
 infile = resp.text
 lines = infile.split('\n')
 lines.pop()
