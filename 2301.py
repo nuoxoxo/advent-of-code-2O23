81f19dc639cc7e1eb7681f128a14847e395f68ba
uxn <malcolm.nuo.xu@gmail.com>
2023-12-01 10:04:50 +0100

    day 1 : cleanup

--- 2301.py
+++ 2301.py
@@ -19,25 +19,27 @@ lines.pop()
 r1 = 0
 r2 = 0
    
-D = {'one' : 1, 'two' : 2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '0':0}
-A = []
+D = {'one' : 1, 'two' : 2, 'three':3, 'four':4, 'five':5, \
+    'six':6, 'seven':7, 'eight':8, 'nine':9, \
+    '1':1, '2':2, '3':3, '4':4, '5':5, \
+    '6':6, '7':7, '8':8, '9':9, '0':0}
 for line in lines:
     n = re.findall(r'\d+', line)
     s = ''.join(n)
     ss = int(s[0] + s[-1])
     r1 += ss
-    temp = []
+    a = []
     for w in D:
         first = line.find(w)
         last = line.rfind(w)
         if (first != -1):
-            temp.append((first, w))
+            a.append((first, w))
         if (last != -1): 
-            temp.append((last, w))
-    A.append(sorted(temp))
-for a in A:
-    L = str(D[a[0][1]])
-    R = str(D[a[-1][1]])
+            a.append((last, w))
+    aa = sorted(a)
+    L = str(D[aa[0][1]])
+    R = str(D[aa[-1][1]])
     r2 += int(L + R)
 print("Star 1:", r1)
 print("Star 2:", r2)
+
