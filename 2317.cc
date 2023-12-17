326095d33a3035311f355c3c779bcdfb1c92b2d0
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-12-17 11:19:05 +0100

    + solving day 17 in cc . fi

--- 2317.cc
+++ 2317.cc
@@ -1,27 +1,25 @@
 //  Dijkstra (queue) tells you the fewest steps we can take
 //  Dijkstra uses a priority queue and chooses the cheapest path each time
-
-//  priority queue ~ heapq
-
 // borrowing code from 2021/day/15
 
 #include "iostream"
 #include "queue"
-#include "utility"
+//#include "utility"
 #include "vector"
 #include "sstream"
+#include "cassert"
 #include "set"//map"
 #include "cstdarg"//"initializer_list"
 using namespace std;
-using pii = pair<int, int>;
-using ppii = pair<pii, pii>;
+//using pii = pair<int, int>;
+//using ppii = pair<pii, pii>;
 using vi = vector<int>;
 using vvi = vector<vector<int>>;
 
 void DBG(vvi&);
 void print(int,...);
-int Dijkstra (vector<vector<int>> &);
-int Dijkstra2 (vector<vector<int>> &);
+
+int DIJK (vvi &, bool);
 
 int main ()
 {
@@ -35,14 +33,18 @@ int main ()
         while (ss >> chr) temp.push_back(chr - '0');
         A.push_back(temp);
     }
+
     //DBG(A);
-    int p1 = Dijkstra(A);
+
+    int p1 = DIJK (A, false);
     cout << "Part 1: " << p1 << endl;
-    int p2 = Dijkstra2(A);
+
+    int p2 = DIJK (A, true);
     cout << "Part 2: " << p2 << endl;
+
 }
 
-int Dijkstra2 (vector<vector<int>> & A)
+int DIJK (vector<vector<int>> & A, bool p2)
 {
     set<vi> seen;
     auto cmp = [](const vi & L, const vi & R) {
@@ -69,18 +71,12 @@ int Dijkstra2 (vector<vector<int>> & A)
         Q.pop();
         if (r > R - 1 || r < 0 || c < 0 || c > C - 1)
             continue ;
-        /*
-        if (r == R - 1 && c == C - 1) // PART 1
-        */
-        if (r == R - 1 && c == C - 1 && step > 3) // PART 2
+        if (r == R - 1 && c == C - 1 && step > (p2 ? 3 : -1)) // PART 2
             break ;
         if (seen.find( { step, r, c, dr, dc } ) != seen.end())
             continue ;
         seen.insert({step, r, c, dr, dc } );
-        /*
-        if (!r && !c || step > 2) // PART 1
-        */
-        if (!r && !c || step > 9) // PART 2
+        if (!r && !c || step > (p2 ? 9 : 2)) // PART 2
         {
             step = 1;
             i = -1;
@@ -97,7 +93,6 @@ int Dijkstra2 (vector<vector<int>> & A)
                     Q.push( { newloss, step, newr, newc, rr, cc } );
                 }
             }
-            //cout << "sizeof Q: " << Q.size() << "\n";
             continue ;
         }
         // cont. curr dir
@@ -108,7 +103,7 @@ int Dijkstra2 (vector<vector<int>> & A)
             Q.push( { newloss, step + 1, newr, newc, dr, dc } );
         }
         // move sideways
-        if (step > 3) // PART 2
+        if (step > (p2 ? 3 : -1)) // PART 2
         {
             step = 1;
             i = -1;
@@ -129,89 +124,6 @@ int Dijkstra2 (vector<vector<int>> & A)
     return loss;
 }
 
-
-int Dijkstra (vector<vector<int>> & A)
-{
-    set<vi> seen;
-    auto cmp = [](const vi & L, const vi & R) {
-        return L[0] > R[0];
-    };
-    priority_queue< vi, vvi, decltype( cmp ) > Q( cmp);
-    //priority_queue<vector<int>> Q;
-    vector<int>::iterator it;
-    int R = A.size(), C = A[0].size();
-    int r = 0, c = 0, dr = 0, dc = 0, loss = A[0][0], step = 0;
-    int newloss, newr, newc, rr, cc, i;
-    vi DR{1,0,-1, 0};
-    vi DC{0,1, 0,-1};
-    Q.push({0,step,r,c,dr,dc}); // loss, step, r, c, dr, dc
-    while ( ! Q.empty())
-    {
-        vi temp = Q.top() ;
-        loss = temp[0],
-        step = temp[1],
-        r = temp[2],
-        c = temp[3],
-        dr = temp[4],
-        dc = temp[5];
-        Q.pop();
-        if (r > R - 1 || r < 0 || c < 0 || c > C - 1)
-            continue ;
-        if (r == R - 1 && c == C - 1)
-            break ;
-        if (seen.find( { step, r, c, dr, dc } ) != seen.end())
-            continue ;
-        seen.insert({step, r, c, dr, dc } );
-
-        //cout << "here\n";
-        //print(6, loss, step, r, c, dr, dc);
-
-        if (!r && !c || step > 2)
-        {
-            step = 1;
-            i = -1;
-            while (++i < 4)
-            {
-                //print(7, loss, step, r, c, dr, dc, i);
-                rr = DR[i], cc = DC[i];
-                if ( rr == dr && cc == dc || rr == -dr && cc == -dc )
-                    continue ;
-                newr = r + rr, newc = c + cc;
-                if ( ! (newr > R - 1 || newr < 0 || newc < 0 || newc > C - 1) )
-                {
-                    newloss = loss + A[newr][newc];
-                    Q.push( { newloss, step, newr, newc, rr, cc } );
-                }
-            }
-            //cout << "sizeof Q: " << Q.size() << "\n";
-            continue ;
-        }
-        // cont. curr dir
-        newr = r + dr, newc = c + dc;
-        if ( ! (newr > R - 1 || newr < 0 || newc < 0 || newc > C - 1) )
-        {
-            newloss = loss + A[newr][newc];
-            Q.push( { newloss, step + 1, newr, newc, dr, dc } );
-        }
-        // move sideways
-        step = 1;
-        i = -1;
-        while (++i < 4)
-        {
-            rr = DR[i], cc = DC[i];
-            if (rr == dr && cc == dc || rr == -dr && cc == -dc)
-                continue ;
-            newr = r + rr, newc = c + cc;
-            if ( ! (newr > R - 1 || newr < 0 || newc < 0 || newc > C - 1) )
-            {
-                newloss = loss + A[newr][newc];
-                Q.push( { newloss, step, newr, newc, rr, cc } );
-            }
-        }
-    }
-    return loss;
-}
-
 void DBG( vvi & A) { for ( vi & a: A) { for (int n: a) cout << n << ' '; cout << '\n'; }}
 void print(int total,...) {
     va_list args;
@@ -221,3 +133,4 @@ void print(int total,...) {
     va_end( args );
     cout << '\n';
 }
+
