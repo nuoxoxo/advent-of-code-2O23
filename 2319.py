7be263e96eee5fe8c38c040bce532474e6c6eeb1
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-12-20 13:58:51 +0100

    day 19 revisited: fi. part 2

--- 2319.py
+++ 2319.py
@@ -2,12 +2,13 @@ F = 0
 r1, r2 = 0, 0
 A = []
 from collections import defaultdict
+import math # part 2
 def DBG_( Dict ):
     for k,v in Dict.items(): print(k,v)
     print()
 def Printer(*args):
     for arg in args: print(arg, end=' ')
-    print()
+    print('')
 
 D = defaultdict(list)
 flow, todo, up, down = [],[],None,None
@@ -28,7 +29,7 @@ for line in up:
         D[name].append([ key, op, val, dest ])
     D[name].append(end)
 
-#DBG_( D )
+DBG_( D )
 
 for line in down:
     flow = defaultdict(int)
@@ -36,7 +37,7 @@ for line in down:
     for xpr in line:
         key, val = xpr.split('=')
         flow[key] = int(val)
-    #DBG_(flow)
+    # DBG_(flow)
     res = None
     curr = 'in'
     found = False
@@ -71,7 +72,60 @@ for line in down:
         #DBG_(flow)
         r1 += sum(flow.values())
 
-print("part 1:", r1)
-print("part 2:", r2)
+Printer("part 1:", r1)
 
+# Part 2
 
+instructions = [
+    ('in', [range(1,4001)] * 4)
+]
+#Printer(instructions)
+
+AC = 0
+while instructions :
+    inst = instructions.pop(0)
+    curr, rangex4 = inst
+    #Printer('4 ranges/', curr, rangex4)
+    if curr in 'AR':
+        if curr == 'A': # case: R/ do nothing and pass; A/ add :in all combinations
+            lengths = [len(_) for _ in rangex4]
+            lengths_product = 1
+            for n in lengths: lengths_product *= n
+            AC += lengths_product
+        continue
+    *conditions, endpoint = D[curr]
+    #Printer('D[curr]/',D[curr],)
+    #Printer('subdict_xcpt_dest/', conditions, '\nendpoint/',endpoint)
+    for key, op, val, dest in conditions:
+        idx = 'xmas'.index(key)
+        _range = rangex4[ idx ]
+        #Printer('key,rangex4[key]/',key,rangex4[idx],'of',rangex4)
+        if val not in _range:
+            continue
+        # split in half
+        # make a range Object (1/2): prepare 2 slice each of which: [2]int
+        L,R = [None]*2, [None]*2
+        if op == '>':
+            L = [ _range.start, val + 1 ]
+            R = [ val + 1, _range.stop ]
+            L,R = R,L
+        elif op == '<':
+            L = [ _range.start, val ]
+            R = [ val, _range.stop ]
+        else:
+            die('wtf')
+        # create a range object (2/2) : L and R are now objects of type range
+        idx = 'xmas'.index(key)
+        L, R = range(*L),range(*R)
+        updated_range_x4 = [_ for _ in rangex4]
+        updated_range_x4 [idx] = L
+        #Printer(L, R)
+        instructions.append( ( dest, updated_range_x4 ) )
+        rangex4[ idx ] = R
+    instructions.append ( (endpoint, rangex4) )
+
+r2 = AC
+
+Printer("part 2:", r2)
+assert(r1 in [19114, 353553])
+assert(r2 in [124615747767410, 167409079868000])
