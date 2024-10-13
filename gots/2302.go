f4b32fd2af8b8d553954e25d720148d4a1b51b75
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-10-13 11:07:39 +0200

    2/done redo

--- gots/2302.go
+++ gots/2302.go
@@ -1,74 +1,48 @@
 package main
 
 import (
-    "bufio"
     "fmt"
-    "os"
     "strings"
     "strconv"
 )
 
+const URL string = "https://adventofcode.com/2023/day/2/input"
+var D = map[string]int{ "red": 12, "blue": 14, "green": 13, }
+
 func main() {
-
-    D := map[string]int{
-
-        "red": 12,
-        "blue": 14,
-        "green": 13,
-    }
-
-    r1, r2 := 0, 0
+    res1, res2 := 0, 0
     idx := 1
-    scanner := bufio.NewScanner(os.Stdin)
-    for scanner.Scan() {
-
-        line := scanner.Text()
+    lines := strings.Split((strings.TrimSpace(string(getbody(URL)))), "\n")
+    for _, line := range lines {
         temp := strings.Split(line, ":")[1]
         words := strings.Split(temp, ";")
         ok := true // part 1
         R, G, B := 0, 0, 0 // part 2
         for _, w := range words {
             sub := strings.Split(w, " ")[1:]
-            i := 0
-            for i < len(sub) - 1 {
+            for i := 0; i < len(sub) - 1; i += 2 {
                 number, color := sub[i], sub[i + 1]
                 if color[len(color) - 1] == ',' {
                     color = color[ : len(color) - 1]
                 }
                 n, _ := strconv.Atoi( number )
                 // part 1
-                if n > D[color] {
-                    ok = false
-                }
+                if n > D[color] { ok = false }
                 // part 2
-                if color == "red" {
-                    R = max(R, n)
-                }
-                if color == "green" {
-                    G = max(G, n)
-                }
-                if color == "blue" {
-                    B = max(B, n)
-                }
-                i += 2
+                if color == "red" { R = max(R, n) }
+                if color == "blue" { B = max(B, n) }
+                if color == "green" { G = max(G, n) }
             }
         }
         // part 1
-        if ok {
-            r1 += idx
-        }
+        if ok { res1 += idx }
         // part 2
-        r2 += R * B * G
+        res2 += R * B * G
         idx += 1
     }
-
-    fmt.Println("Part 1: ", r1)
-    fmt.Println("Part 2: ", r2)
+    fmt.Println(YELL("part 1:"), res1)
+    fmt.Println(YELL("part 2:"), res2)
+    assert (res1 == 2085)
+    assert (res2 == 79315)
 }
 
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
